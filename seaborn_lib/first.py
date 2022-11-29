3c42a4917770791808bc3ab9ae2e8f796f03e466
omkashyap007 <omkashyapcric@gmail.com>
2022-11-29 18:20:57 +0530

    Change in color

--- seaborn_lib/first.py
+++ seaborn_lib/first.py
@@ -8,5 +8,5 @@ seaborn.set(style= "whitegrid")
 x = [random.randint(1,11)   for _ in range(20)]
 y = [random.randint(1,11)   for _ in range(20)]
 seaborn.boxplot(x  = x  ,y = y ,notch= True ,flierprops = {"marker"  : "x" } , 
-                boxprops={"facecolor": (0,1,0,1)})
+                boxprops={"facecolor": (0,1,1,1)})
 mlt.show()
