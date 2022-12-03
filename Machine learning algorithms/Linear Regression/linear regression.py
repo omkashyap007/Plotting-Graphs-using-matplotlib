d76a4a6a7ea8be2cfb5419001dfac50e8b438144
omkashyap007 <omkashyapcric@gmail.com>
2022-12-04 00:39:39 +0530

    Added Machine Learning algorithms

---
+++ Machine learning algorithms/Linear Regression/linear regression.py
@@ -0,0 +1,6 @@
+import sklearn
+from sklearn import datasets , linear_model
+from sklearn.metrics import r2_score
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
